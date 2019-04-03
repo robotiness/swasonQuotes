2af57801ecd5720b93a8760cdcf69c9255b64dc5
Christopher McCoy <cmmccoy1996@gmail.com>
2019-04-03 10:27:24 -0700

    t

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ var mongoose=require('mongoose');
 var path=require('path');
 var app=express();
 var PORT=require('./config').PORT;
-var mongoKey=require('./config').MONGODB_KEY;
+//var mongoKey=require('./config').MONGODB_KEY;
 
+if(process.env.port)
+{
+    PORT=process.env.port;
+    mongoKey=process.env.MONGODB_KEY;
+
+}
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect(mongoKey);
 app.use(quotesModule,voteModule);
-if(process.env.port)
-{
-    PORT=process.env.port;
-}
+
 app.listen(PORT,function(req,res){
     console.log("Listening on port"+PORT);
 });
